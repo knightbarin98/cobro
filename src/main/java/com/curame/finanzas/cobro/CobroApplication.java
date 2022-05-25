e40f1feb631680c82f40d91d3783c5e29a26ade2
ivansoto <lopezsotoivan01@gmail.com>
2022-05-24 19:16:46 -0500

    first commit

---
+++ src/main/java/com/curame/finanzas/cobro/CobroApplication.java
@@ -0,0 +1,13 @@
+package com.curame.finanzas.cobro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CobroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CobroApplication.class, args);
+	}
+
+}
